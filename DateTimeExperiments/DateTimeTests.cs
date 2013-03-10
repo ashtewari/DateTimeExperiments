f64346524d703bbc4be62b0edea8ff1ab39f90c2
Ash Tewari <ash.tewari.name@gmail.com>
2013-03-10 17:41:28 -0400

    CreateCustomTimeZoneInfoWithDstRules

--- DateTimeExperiments/DateTimeTests.cs
+++ DateTimeExperiments/DateTimeTests.cs
@@ -35,6 +35,20 @@ namespace DateTimeExperiments
             Assert.IsFalse(isDst);
         }
 
+        /// <summary>
+        /// Compares the custom time zone info with default.
+        /// </summary>
+        [TestMethod]
+        public void CompareCustomTimeZoneInfoWithDefault()
+        {
+            var ts = new DateTime(1980, 4, 15, 12, 0, 0);
+            
+            var isDstDefault = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time").IsDaylightSavingTime(ts);
+            var isDstCustom = Utils.CreateCustomTimeZoneInfoWithDstRules().IsDaylightSavingTime(ts);
+
+            Assert.IsTrue(isDstDefault);
+            Assert.IsFalse(isDstCustom);
+        }
 
         /// <summary>
         /// Test is day light savings time using noda time.
@@ -43,18 +57,17 @@ namespace DateTimeExperiments
         public void Test_NodaTime_IsDayLightSavingTime()
         {
             var ts = new DateTime(1980, 4, 15, 12, 0, 0);
-
-            var local = LocalDateTime.FromDateTime(ts);
-            var zdt = Utils.TzEast.ResolveLocal(local, Utils.CustomResolver);
-
-            Assert.IsFalse(zdt.IsDaylightSavingTime());
+            
+            var isDst = Utils.IsDaylightSavingTime(ts);
+            
+            Assert.IsFalse(isDst);
         }
 
         [TestMethod]
         // Out of DST (According to 1987 rules, DST would have started on 4/5/1970, but it started on 4/26/1970 according to rules actually in place in 1970.)
         public void Test_1970_Out_DST()
         {
-            var dateString = "1970-04-15 12:00:00.000000";
+            //// var dateString = "1970-04-15 12:00:00.000000";
             var expectedValue = "1970-04-15 17:00:00.000000";
 
             var ts = new DateTime(1970, 4, 15, 12, 0, 0);            
@@ -63,5 +76,31 @@ namespace DateTimeExperiments
 
             Assert.AreEqual(expectedValue, actualValue);
         }
+
+        [TestMethod]
+        public void AddAdjustmentRulesToEasternTimeZone()
+        {
+            var rules = Utils.EasternTimeZone.GetAdjustmentRules();
+            
+            foreach (var rule in rules)
+            {
+                Console.WriteLine(string.Format("{0} [{2}] {1} : {3} > {4}", rule.DateStart, rule.DateEnd, rule.DaylightDelta, rule.DaylightTransitionStart, rule.DaylightTransitionEnd)); 
+            }
+
+            var isDst = false;
+            isDst = Utils.EasternTimeZone.IsDaylightSavingTime(new DateTime(1980, 3, 31));
+            isDst = Utils.EasternTimeZone.IsDaylightSavingTime(new DateTime(1980, 4, 15));
+            isDst = Utils.EasternTimeZone.IsDaylightSavingTime(new DateTime(1980, 4, 30));
+
+            isDst = Utils.IsDaylightSavingTime(new DateTime(1980, 3, 31));
+            isDst = Utils.IsDaylightSavingTime(new DateTime(1980, 4, 15));
+            isDst = Utils.IsDaylightSavingTime(new DateTime(1980, 4, 30));
+
+            var etz = Utils.CreateCustomTimeZoneInfoWithDstRules();
+            isDst = etz.IsDaylightSavingTime(new DateTime(1980, 3, 31));
+            isDst = etz.IsDaylightSavingTime(new DateTime(1980, 4, 15));
+            isDst = etz.IsDaylightSavingTime(new DateTime(1980, 4, 30));
+
+        }        
     }
 }
--- DateTimeExperiments/Utils.cs
+++ DateTimeExperiments/Utils.cs
@@ -1,6 +1,7 @@
 ﻿namespace DateTimeExperiments
 {
     using System;
+    using System.Collections.Generic;
 
     using NodaTime;
     using NodaTime.TimeZones;
@@ -43,6 +44,121 @@
             var local = LocalDateTime.FromDateTime(ts);
             var zdt = BclEast.ResolveLocal(local, CustomResolver);
             return zdt.ToDateTimeUtc();
+        }
+
+        public TimeZoneInfo CreateEasternTimeZoneWithCompleteAdjustmentRules()
+        {
+            return null;
+        }
+
+        public static bool IsDaylightSavingTime(DateTime ts)
+        {
+            var local = LocalDateTime.FromDateTime(ts);
+            var zdt = Utils.TzEast.ResolveLocal(local, Utils.CustomResolver);
+            return zdt.IsDaylightSavingTime();
+        }
+
+        /// <summary>
+        /// Creates the custom time zone info with DST rules.
+        /// </summary>
+        /// <returns>The custom time zone.</returns>
+        public static TimeZoneInfo CreateCustomTimeZoneInfoWithDstRules()
+        {
+            // Clock is adjusted one hour forward or backward
+            var delta = new TimeSpan(1, 0, 0);
+
+            //This will hold all the DST adjustment rules
+            var listOfAdjustments = new List<TimeZoneInfo.AdjustmentRule>();
+
+            /*
+                Rule	US	1918	1919	-	Mar	lastSun	2:00	1:00	D
+                Rule	US	1918	1919	-	Oct	lastSun	2:00	0	S             
+             */
+            var ruleStart = TimeZoneInfo.TransitionTime.CreateFloatingDateRule(new DateTime(1, 1, 1, 2, 0, 0), 03, 05, DayOfWeek.Sunday);
+            var ruleEnd = TimeZoneInfo.TransitionTime.CreateFloatingDateRule(new DateTime(1, 1, 1, 2, 0, 0), 10, 05, DayOfWeek.Sunday);
+            var adjustment = TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(new DateTime(1918, 1, 1), new DateTime(1919, 12, 31), delta,
+                                                                       ruleStart, ruleEnd);
+            listOfAdjustments.Add(adjustment);
+
+            /*
+                Rule	US	1942	only	-	Feb	9	2:00	1:00	W # War 
+             */
+            ruleStart = TimeZoneInfo.TransitionTime.CreateFixedDateRule(new DateTime(1, 1, 1, 2, 0, 0), 02, 09);
+            adjustment = TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(new DateTime(1942, 1, 1), new DateTime(1942, 12, 31),
+                                                                       delta, ruleStart, ruleEnd);
+            listOfAdjustments.Add(adjustment);
+
+            /*
+                Rule	US	1945	only	-	Aug	14	23:00u	1:00	P # Peace
+                Rule	US	1945	only	-	Sep	30	2:00	0	S
+             */
+            ruleStart = TimeZoneInfo.TransitionTime.CreateFixedDateRule(new DateTime(1, 1, 1, 23, 0, 0), 08, 14);
+            ruleEnd = TimeZoneInfo.TransitionTime.CreateFixedDateRule(new DateTime(1, 1, 1, 2, 0, 0), 09, 30);
+            adjustment = TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(new DateTime(1945, 1, 1), new DateTime(1945, 12, 31),
+                                                                       delta, ruleStart, ruleEnd);
+            listOfAdjustments.Add(adjustment);
+
+            /*
+                Rule	US	1967	2006	-	Oct	lastSun	2:00	0	S             
+             */
+            ruleEnd = TimeZoneInfo.TransitionTime.CreateFloatingDateRule(new DateTime(1, 1, 1, 2, 0, 0), 10, 5, DayOfWeek.Sunday);
+
+            /*
+                Rule	US	1967	1973	-	Apr	lastSun	2:00	1:00	D
+             */
+            ruleStart = TimeZoneInfo.TransitionTime.CreateFloatingDateRule(new DateTime(1, 1, 1, 2, 0, 0), 04, 05, DayOfWeek.Sunday);
+            adjustment = TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(new DateTime(1967, 1, 1), new DateTime(1973, 12, 31),
+                                                                       delta, ruleStart, ruleEnd);
+            listOfAdjustments.Add(adjustment);
+
+            /*
+                Rule	US	1974	only	-	Jan	6	2:00	1:00	D
+             */
+            ruleStart = TimeZoneInfo.TransitionTime.CreateFixedDateRule(new DateTime(1, 1, 1, 2, 0, 0), 01, 06);
+            adjustment = TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(new DateTime(1974, 1, 1), new DateTime(1974, 12, 31),
+                                                                       delta, ruleStart, ruleEnd);
+            listOfAdjustments.Add(adjustment);
+
+
+            /*
+                Rule	US	1975	only	-	Feb	23	2:00	1:00	D             
+             */
+            ruleStart = TimeZoneInfo.TransitionTime.CreateFixedDateRule(new DateTime(1, 1, 1, 2, 0, 0), 02, 23);
+            adjustment = TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(new DateTime(1975, 1, 1), new DateTime(1975, 12, 31),
+                                                                       delta, ruleStart, ruleEnd);
+            listOfAdjustments.Add(adjustment);
+
+            /*
+                Rule	US	1976	1986	-	Apr	lastSun	2:00	1:00	D             
+             */
+            ruleStart = TimeZoneInfo.TransitionTime.CreateFloatingDateRule(new DateTime(1, 1, 1, 2, 0, 0), 04, 05, DayOfWeek.Sunday);
+            adjustment = TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(new DateTime(1976, 1, 1), new DateTime(1986, 12, 31),
+                                                                       delta, ruleStart, ruleEnd);
+            listOfAdjustments.Add(adjustment);
+
+            /*
+                Rule	US	1987	2006	-	Apr	Sun>=1	2:00	1:00	D             
+             */
+            ruleStart = TimeZoneInfo.TransitionTime.CreateFloatingDateRule(new DateTime(1, 1, 1, 2, 0, 0), 04, 01, DayOfWeek.Sunday);
+            adjustment = TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(new DateTime(1987, 1, 1), new DateTime(2006, 12, 31),
+                                                                       delta, ruleStart, ruleEnd);
+            listOfAdjustments.Add(adjustment);
+
+            /*
+                Rule	US	2007	max	-	Mar	Sun>=8	2:00	1:00	D
+                Rule	US	2007	max	-	Nov	Sun>=1	2:00	0	S             
+             */
+            ruleStart = TimeZoneInfo.TransitionTime.CreateFloatingDateRule(new DateTime(1, 1, 1, 2, 0, 0), 03, 02, DayOfWeek.Sunday);
+            ruleEnd = TimeZoneInfo.TransitionTime.CreateFloatingDateRule(new DateTime(1, 1, 1, 2, 0, 0), 11, 01, DayOfWeek.Sunday);
+            adjustment = TimeZoneInfo.AdjustmentRule.CreateAdjustmentRule(new DateTime(2007, 1, 1), DateTime.MaxValue.Date,
+                                                                       delta, ruleStart, ruleEnd);
+            listOfAdjustments.Add(adjustment);
+            
+            var adjustments = new TimeZoneInfo.AdjustmentRule[listOfAdjustments.Count];
+            listOfAdjustments.CopyTo(adjustments);
+
+            return TimeZoneInfo.CreateCustomTimeZone("Custom Eastern Standard Time", new TimeSpan(-5, 0, 0),
+                  "(GMT-05:00) Eastern Time (US Only)", "Eastern Standard Time", "Eastern Daylight Time", adjustments);
         }
     }
 }
